80e6322b67a7f987e4565763d51010113fe91f3c
Ankit Shrestha <ankit.shrestha196@gmail.com>
2022-06-29 22:18:31 +0545

    Complete vanilla stop watch

--- script.js
+++ script.js
@@ -1,16 +1,45 @@
-const timer = document.querySelector(".timer")
+const timerSec = document.querySelector("#sec")
+const timerTen = document.querySelector("#ten")
 const start = document.querySelector(".start")
 const stop = document.querySelector(".stop")
 const reset = document.querySelector(".reset")
+let sec = 00
+let tens = 00
+let interval
 
-start.addEventListener("click", e => {
-    timer.textContent = 'start'
+
+start.addEventListener("click", () => {
+    clearInterval(interval)
+    interval = setInterval(() => {
+        tens++
+        if (tens <= 9) {
+            timerTen.textContent = "0" + tens
+        } 
+        if (tens > 9) {
+            timerTen.textContent = tens
+        }
+
+        if (tens > 99) {
+            sec += 1
+            timerSec.textContent = "0" + sec
+            tens = 00
+            timerTen.textContent = "0" + tens
+        }
+
+        if (sec > 9) {
+            timerSec.textContent = sec
+        }
+    }, 10)
 })
 
 stop.addEventListener("click", e => {
-    timer.textContent = 'stop'
+    clearInterval(interval)
 })
 
 reset.addEventListener("click", e => {
-    timer.innerText = "00:00"
+    clearInterval(interval)
+    sec = 00
+    tens = 00
+    timerSec.textContent = "00"
+    timerTen.textContent = "00"
 })
